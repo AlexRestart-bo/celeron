54f8578661dc75e154dad812f8f912957b0489a7
alex_butterfly <alex.butterfly@beauty.com>
2025-11-10 14:48:47 -0500

    Reassembling the 'add_employee' function

--- study_pas/src/task4.c
+++ study_pas/src/task4.c
@@ -2,7 +2,13 @@
 
 Employee* create_employees_array(int init_size){
     Employee *worker = malloc(init_size*sizeof(Employee));
-    if (worker == NULL) return -1; 
+    if (worker == NULL) return NULL; 
+    worker->len_name = 1;
+    worker->name = malloc(worker->len_name*sizeof(char));
+    if (worker->name == NULL) return NULL;
+    worker->len_post = 1;
+    worker->post = malloc(worker->len_post*sizeof(char));
+    if (worker->post == NULL) return NULL;
     return worker;
 }
 
@@ -12,68 +18,103 @@ void add_employee(Employee **collaborators, size_t *size_array){
         printf("Не удалось увеличить размер массива (сейчас он состоит из %i элементов)", *size_array);
         return;
     }
+    printf("Введите последовательно через пробел имя, \
+        должность, зарплату и опыт (в днях) сотрудника\n");
     *collaborators = tmp;
     (*size_array)++;
-    char c;
-    //всегда оставляем место для нуль-терминатора
-    char name_counter = 1;
-    //читаем имя до пробела
-    while((c = getchar()) != ' ' && c != EOF && c != '\n'){
-        char *name_tmp = realloc(collaborators[*size_array]->name, (++name_counter)*sizeof(char));
-        if(name_tmp == NULL){
-            collaborators[*size_array]->name[name_counter - 2] = '\0';
-            collaborators[*size_array]->len_name = name_counter - 1;
-            return;
-        }
-        collaborators[*size_array]->name = name_tmp;
-        collaborators[*size_array]->name[name_counter - 2] = c;
-    }
-    collaborators[*size_array]->name[name_counter - 1] = '\0';
-    collaborators[*size_array]->len_name = name_counter;
-    //читаем дожность до пробела
-    char post_counter = 1;
-    while((c = getchar()) != ' ' && c != EOF && c != '\n'){
-        char *post_tmp = realloc(collaborators[*size_array]->post, (++post_counter)*sizeof(char));
-        if(post_tmp == NULL){
-            collaborators[*size_array]->post[post_counter - 2] = '\0';
-            collaborators[*size_array]->len_post = post_counter -1;
-            return;
-        }
-        collaborators[*size_array]->post = post_tmp;
-        collaborators[*size_array]->post[post_counter - 2] = c;
-    }
-    collaborators[*size_array]->post[post_counter - 1] = '\0';
-    collaborators[*size_array]->len_post = name_counter;
-    //после чтения числа как строки оно преобразуется к типу int
-    char salary_counter = 1;
-    char *salary_str = malloc(sizeof(char));
-    while((c = getchar()) != ' ' && c != EOF && c != '\n'){
-        char *salary_tmp = realloc(salary_str, (salary_counter++ + 1)*sizeof(char));
-        if(salary_tmp == NULL){
-            *(salary_str + salary_counter - 2) = '\0';
-            return;
-        }
-        salary_str = salary_tmp;
-        *(salary_str + salary_counter - 2) = c;
-    }
-    *(salary_str + salary_counter - 1) = '\0';
-    collaborators[*size_array]->salary = convert_str_int(salary_str, salary_counter);
-    free(salary_str);
+    //при записи динамической строки последний символ вегда будет нуль-терминатором
+    int len_inpst = 1;
+    char* input_string = malloc(len_inpst*sizeof(char));
+    if(input_string == NULL) return;
+    //чтение вводимой пользователем строки
+    read_line_dynamic(&input_string, &len_inpst);
+    int j = 0;
+    /**
+     * field - номер заполняемого (!!! поля len_name и len_post не являются таковыми) поля структуры
+     * Employee. Меняется в пределах [0, 4].
+    */
+    char field = 0;
+    /**
+     * within - внутренний счетчик для заполнения полей name и post в структуре Employee
+    */
+    char within;
+    char *written_salary = malloc(sizeof(char));
+    if (written_salary == NULL) return;
 
-    char exper_counter = 1;
-    char *exper_str = malloc(sizeof(char));
-    while((c = getchar()) != ' ' && c != EOF && c != '\n'){
-        char *exper_tmp = realloc(exper_str, (exper_counter++ + 1)*sizeof(char));
-        if(exper_tmp == NULL){
-            *(exper_str + exper_counter - 2) = '\0';
-            return;
+    char *written_expr = malloc(sizeof(char));
+    if (written_expr == NULL) return;
+    /**
+     * переменные index_wrt_salary и index_wrt_expr означают лишь порядок в строковом массиве 
+     * (0, 1, ..., index_wrt_salary) written_salary[] и written_expr[] соответственно. Значит,
+     * размер этих массивов будет составлять index_wrt_salary + 1 и index_wrt_expr + 1 
+     * соответственно.
+    */
+    char index_wrt_salary = 0;
+    char index_wrt_expr = 0;
+    /**
+     * в цикле j < len_inpst && input_string[j] != '\0' первое условие выполняется вплоть до последнего
+     * элемента j = len_inpst - 1, в то же время последним эллементом является нуль-терминатор, а
+     * предспоследним символ ' ' (пробел) - см. функцию read_line_dinamic(). 
+     * */ 
+    
+    while(j < len_inpst && input_string[j] != '\0'){
+        if (input_string[j] == ' ') {
+            field++;
+            within = 0;
         }
-        exper_str = exper_tmp;
-        *(exper_str + exper_counter - 2) = c;
+        switch(field){
+            case 0:
+                if (within < 100){
+                    char *tmp_name = realloc(collaborators[*size_array]->name, 
+                        (collaborators[*size_array]->len_name + 1)*sizeof(char));
+                    if (tmp_name == NULL) return;
+                    collaborators[*size_array]->name = tmp_name;
+                    collaborators[*size_array]->len_name++;
+                    collaborators[*size_array]->name[within++] = input_string[j];
+                } 
+                break;
+            case 1:
+                if(within == 0) collaborators[*size_array]->name[collaborators[*size_array]->len_name - 1] = '\0';
+                if (within < 100){
+                    char *tmp_post = realloc(collaborators[*size_array]->post, 
+                        (collaborators[*size_array]->len_post + 1)*sizeof(char));
+                    if (tmp_post == NULL) return;
+                    collaborators[*size_array]->post = tmp_post;
+                    collaborators[*size_array]->len_post++;
+                    collaborators[*size_array]->post[within++] = input_string[j];
+                } 
+                break;
+            case 2:
+                if(index_wrt_salary == 0)
+                    collaborators[*size_array]->post[collaborators[*size_array]->len_post - 1] = '\0';
+                if (index_wrt_salary < 9){
+                    char *tmp_wrtsl = realloc(written_salary, (index_wrt_salary + 1)*sizeof(char));
+                    if (tmp_wrtsl == NULL) return;
+                    written_salary = tmp_wrtsl;
+                    written_salary[index_wrt_salary++] = input_string[j];
+                }
+                break;
+            case 3:
+                if (index_wrt_expr < 9){
+                    char *tmp_wrtex = realloc(written_expr, (index_wrt_expr + 1)*sizeof(char));
+                    if (tmp_wrtex == NULL) return;
+                    written_expr = tmp_wrtex;
+                    written_expr[index_wrt_expr++] = input_string[j];
+                }
+                break;
+            case 4:
+                // небязательный шаг
+                written_salary[index_wrt_salary] = '\0';
+                written_expr[index_wrt_expr] = '\0';
+                collaborators[*size_array]->salary = convert_str_int(written_salary, index_wrt_salary + 1);
+                collaborators[*size_array]->experience = convert_str_int(written_expr, index_wrt_expr + 1);
+                free(written_salary);
+                free(written_expr);
+                break;
+        }
+        j++;
     }
-    *(exper_str + exper_counter - 1) = '\0';
-    collaborators[*size_array]->experience = convert_str_int(exper_str, exper_counter);
-    free(exper_str);
+    free(input_string);
 }
 
 
@@ -93,9 +134,7 @@ void add_employee(Employee **collaborators, size_t *size_array){
  *  с помощью realloc().
  */
 
-void remove_employee(Employee **collaborators, size_t *size_array, int index){
-
-}
+void remove_employee(Employee **collaborators, size_t *size_array, int index){}
 
 void search_by_name(){}
 
